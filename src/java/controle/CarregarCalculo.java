b43a90284db1a0af14fcc6c2af4f787e37613c57
Guilherme da Luz <guilherme.daluz@outlook.com>
2014-03-27 20:18:10 -0300

    Indentation and comments corrections
    Corrections of indentation according to the Code Conventions document
    and few comments.

--- src/java/controle/CarregarCalculo.java
+++ src/java/controle/CarregarCalculo.java
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
  */
 package controle;
 
@@ -22,167 +22,202 @@ import modelo.Subtrair;
 import modelo.Transpor;
 
 /**
- *
+ * 
  * @author Andre
  */
 public class CarregarCalculo extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        HttpSession session = request.getSession();
-        try {
-        if(session.getAttribute("calculo") == null){
-        response.sendRedirect("index.jsp?erro=1");
-        }else{
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CarregarCalculo</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            String operacao;
-            try {
-                int id  = Integer.parseInt(request.getParameter("id"));
-                CalculoDAO cDB = new CalculoDAO();
-                cDB.conectar();
-                Calculo  calculo = cDB.carregaPorId(id);
-                operacao = calculo.getOperacao();
-        if(operacao.equals("Inverter")){
-            Inverter i = (Inverter) calculo;
-            i.setDadosString();
-            session.setAttribute("dados_t_a", i.getEntrada());
-            session.setAttribute("dados_inversa_dima", i.getDimensaoA());
-            session.setAttribute("dados_inversa_dimb", i.getDimensaoB());
-            out.print("<script language='JavaScript'>");
-            out.print(" window.open('altera_inversa.jsp?id="+i.getId()+"','_parent');");
-            out.print("</script>");
-        }else if(operacao.equals("Transpor")){
-            Transpor t = (Transpor) calculo;
-            t.setDadosString();
-            session.setAttribute("dados_transposta_a", t.getEntrada());
-            session.setAttribute("dados_transposta_dima", t.getDimensaoA());
-            session.setAttribute("dados_transposta_dimb", t.getDimensaoB());
-            out.print("<script language='JavaScript'>");
-            out.print(" window.open('altera_transposta.jsp?id="+t.getId()+"','_parent');");
-            out.print("</script>");
-        }else if(operacao.equals("Determinar")){
-            Determinar d = (Determinar) calculo;
-            d.setDadosString();
-            session.setAttribute("dados_determinante_a", d.getEntrada());
-            session.setAttribute("dados_determinante_dima", d.getDimensaoA());
-            session.setAttribute("dados_determinante_dimb", d.getDimensaoB());
-            out.print("<script language='JavaScript'>");
-            out.print(" window.open('altera_determinante.jsp?id="+d.getId()+"','_parent');");
-            out.print("</script>");
-        }else if(operacao.equals("Somar")){
-            Somar s = (Somar) calculo;
-            s.setDadosString();
-            session.setAttribute("dados_soma_a", s.getEntradaA());
-            session.setAttribute("dados_soma_b", s.getEntradaB());
-            session.setAttribute("dados_soma_dima", s.getDimensaoA());
-            session.setAttribute("dados_soma_dimb", s.getDimensaoB());
-            session.setAttribute("dados_soma_dimc", s.getDimensaoA());
-            session.setAttribute("dados_soma_dimd", s.getDimensaoB());
-            out.print("<script language='JavaScript'>");
-            out.print(" window.open('altera_soma.jsp?id="+s.getId()+"','_parent');");
-            out.print("</script>");
-        }else if(operacao.equals("Subtrair")){
-            Subtrair s = (Subtrair) calculo;
-            s.setDadosString();
-            session.setAttribute("dados_subtrai_a", s.getEntradaA());
-            session.setAttribute("dados_subtrai_b", s.getEntradaB());
-            session.setAttribute("dados_subtrai_dima", s.getDimensaoA());
-            session.setAttribute("dados_subtrai_dimb", s.getDimensaoB());
-            session.setAttribute("dados_subtrai_dimc", s.getDimensaoA());
-            session.setAttribute("dados_subtrai_dimd", s.getDimensaoB());
-            out.print("<script language='JavaScript'>");
-            out.print(" window.open('altera_subtrai.jsp?id="+s.getId()+"','_parent');");
-            out.print("</script>");
-        }else if(operacao.equals("Multiplicar")){
-            Multiplicar m = (Multiplicar) calculo;
-            m.setDadosString();
-            session.setAttribute("dados_multiplica_a", m.getEntradaA());
-            session.setAttribute("dados_multiplica_b", m.getEntradaB());
-            session.setAttribute("dados_multiplica_dima", m.getDimensaoA());
-            session.setAttribute("dados_multiplica_dimb", m.getDimensaoB());
-            session.setAttribute("dados_multiplica_dimc", m.getDimensaoB());
-            session.setAttribute("dados_multiplica_dimd", m.getDimensaoC());
-            out.print("<script language='JavaScript'>");
-            out.print(" window.open('altera_multiplica.jsp?id="+m.getId()+"','_parent');");
-            out.print("</script>");
-        }else if(operacao.equals("Escalar")){
-            Escalar e = (Escalar) calculo;
-            e.setDadosString();
-            session.setAttribute("dados_escalar_a", e.getEntradaA());
-            session.setAttribute("dados_escalar_n", e.getEntradaB());
-            session.setAttribute("dados_escalar_dima", e.getDimensaoA());
-            session.setAttribute("dados_escalar_dimb", e.getDimensaoB());
-            out.print("<script language='JavaScript'>");
-            out.print(" window.open('altera_escalar.jsp?id="+e.getId()+"','_parent');");
-            out.print("</script>");
-        }
-                
-            } catch (Exception x) {
-                x.printStackTrace();
-            }
-            out.println("</body>");
-            out.println("</html>");
-        }
-        } finally {            
-            out.close();
-        }
+    protected void processRequest(HttpServletRequest request,
+				  HttpServletResponse response)
+	    throws ServletException, IOException {
+	response.setContentType("text/html;charset=UTF-8");
+	PrintWriter out = response.getWriter();
+	HttpSession session = request.getSession();
+	try {
+	    if (session.getAttribute("calculo") == null) {
+		response.sendRedirect("index.jsp?erro=1");
+	    } else {
+		/*
+		 * TODO output your page here. You may use following sample
+		 * code.
+		 */
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Servlet CarregarCalculo</title>");
+		out.println("</head>");
+		out.println("<body>");
+		String operacao;
+		try {
+		    int id = Integer.parseInt(request.getParameter("id"));
+		    CalculoDAO cDB = new CalculoDAO();
+		    cDB.conectar();
+		    Calculo calculo = cDB.carregaPorId(id);
+		    operacao = calculo.getOperacao();
+		    if (operacao.equals("Inverter")) {
+			Inverter i = (Inverter) calculo;
+			i.setDadosString();
+			session.setAttribute("dados_t_a", i.getEntrada());
+			session.setAttribute("dados_inversa_dima",
+				i.getDimensaoA());
+			session.setAttribute("dados_inversa_dimb",
+				i.getDimensaoB());
+			out.print("<script language='JavaScript'>");
+			out.print(" window.open('altera_inversa.jsp?id="
+				+ i.getId() + "','_parent');");
+			out.print("</script>");
+		    } else if (operacao.equals("Transpor")) {
+			Transpor t = (Transpor) calculo;
+			t.setDadosString();
+			session.setAttribute("dados_transposta_a",
+				t.getEntrada());
+			session.setAttribute("dados_transposta_dima",
+				t.getDimensaoA());
+			session.setAttribute("dados_transposta_dimb",
+				t.getDimensaoB());
+			out.print("<script language='JavaScript'>");
+			out.print(" window.open('altera_transposta.jsp?id="
+				+ t.getId() + "','_parent');");
+			out.print("</script>");
+		    } else if (operacao.equals("Determinar")) {
+			Determinar d = (Determinar) calculo;
+			d.setDadosString();
+			session.setAttribute("dados_determinante_a",
+				d.getEntrada());
+			session.setAttribute("dados_determinante_dima",
+				d.getDimensaoA());
+			session.setAttribute("dados_determinante_dimb",
+				d.getDimensaoB());
+			out.print("<script language='JavaScript'>");
+			out.print(" window.open('altera_determinante.jsp?id="
+				+ d.getId() + "','_parent');");
+			out.print("</script>");
+		    } else if (operacao.equals("Somar")) {
+			Somar s = (Somar) calculo;
+			s.setDadosString();
+			session.setAttribute("dados_soma_a", s.getEntradaA());
+			session.setAttribute("dados_soma_b", s.getEntradaB());
+			session.setAttribute("dados_soma_dima",
+				s.getDimensaoA());
+			session.setAttribute("dados_soma_dimb",
+				s.getDimensaoB());
+			session.setAttribute("dados_soma_dimc",
+				s.getDimensaoA());
+			session.setAttribute("dados_soma_dimd",
+				s.getDimensaoB());
+			out.print("<script language='JavaScript'>");
+			out.print(" window.open('altera_soma.jsp?id="
+				+ s.getId() + "','_parent');");
+			out.print("</script>");
+		    } else if (operacao.equals("Subtrair")) {
+			Subtrair s = (Subtrair) calculo;
+			s.setDadosString();
+			session.setAttribute("dados_subtrai_a", s.getEntradaA());
+			session.setAttribute("dados_subtrai_b", s.getEntradaB());
+			session.setAttribute("dados_subtrai_dima",
+				s.getDimensaoA());
+			session.setAttribute("dados_subtrai_dimb",
+				s.getDimensaoB());
+			session.setAttribute("dados_subtrai_dimc",
+				s.getDimensaoA());
+			session.setAttribute("dados_subtrai_dimd",
+				s.getDimensaoB());
+			out.print("<script language='JavaScript'>");
+			out.print(" window.open('altera_subtrai.jsp?id="
+				+ s.getId() + "','_parent');");
+			out.print("</script>");
+		    } else if (operacao.equals("Multiplicar")) {
+			Multiplicar m = (Multiplicar) calculo;
+			m.setDadosString();
+			session.setAttribute("dados_multiplica_a",
+				m.getEntradaA());
+			session.setAttribute("dados_multiplica_b",
+				m.getEntradaB());
+			session.setAttribute("dados_multiplica_dima",
+				m.getDimensaoA());
+			session.setAttribute("dados_multiplica_dimb",
+				m.getDimensaoB());
+			session.setAttribute("dados_multiplica_dimc",
+				m.getDimensaoB());
+			session.setAttribute("dados_multiplica_dimd",
+				m.getDimensaoC());
+			out.print("<script language='JavaScript'>");
+			out.print(" window.open('altera_multiplica.jsp?id="
+				+ m.getId() + "','_parent');");
+			out.print("</script>");
+		    } else if (operacao.equals("Escalar")) {
+			Escalar e = (Escalar) calculo;
+			e.setDadosString();
+			session.setAttribute("dados_escalar_a", e.getEntradaA());
+			session.setAttribute("dados_escalar_n", e.getEntradaB());
+			session.setAttribute("dados_escalar_dima",
+				e.getDimensaoA());
+			session.setAttribute("dados_escalar_dimb",
+				e.getDimensaoB());
+			out.print("<script language='JavaScript'>");
+			out.print(" window.open('altera_escalar.jsp?id="
+				+ e.getId() + "','_parent');");
+			out.print("</script>");
+		    }
+
+		} catch (Exception x) {
+		    x.printStackTrace();
+		}
+		out.println("</body>");
+		out.println("</html>");
+	    }
+	} finally {
+	    out.close();
+	}
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // <editor-fold defaultstate="collapsed"
+// desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
+     * Handles the HTTP <code>GET</code> method.
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    protected void doGet(HttpServletRequest request,
+			 HttpServletResponse response) throws ServletException,
+	    IOException {
+	processRequest(request, response);
     }
 
     /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
+     * Handles the HTTP <code>POST</code> method.
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    protected void doPost(HttpServletRequest request,
+			  HttpServletResponse response)
+	    throws ServletException, IOException {
+	processRequest(request, response);
     }
 
     /**
      * Returns a short description of the servlet.
-     *
+     * 
      * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
-        return "Short description";
+	return "Short description";
     }// </editor-fold>
 }
--- src/java/controle/EscalarMatriz.java
+++ src/java/controle/EscalarMatriz.java
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
  */
 package controle;
 
@@ -16,165 +16,169 @@ import modelo.Escalar;
 import modelo.Usuario;
 
 /**
- *
+ * 
  * @author Andre
  */
 public class EscalarMatriz extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        HttpSession session = request.getSession();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EscalarMatriz</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            int i,j,dima=0,dimb=0,erro = 0;
-            double n = 0;
-            if(request.getParameter("dima") != null){
-                try{
-                dima = Integer.parseInt(request.getParameter("dima"));
-                }catch(Exception e){
-                erro = 1;
-                out.print("<script language='JavaScript'>");
-                out.print(" alert('Caracteres proibidos detectados!');");
-                out.print(" window.open('altera_escalar.jsp','_parent');");
-                out.print("</script>");
-                }
-            }
-            if(request.getParameter("dimb") != null){
-                try{
-                dimb = Integer.parseInt(request.getParameter("dimb"));
-                }catch(Exception e){
-                erro = 1;
-                out.print("<script language='JavaScript'>");
-                out.print(" alert('Caracteres proibidos detectados!');");
-                out.print(" window.open('altera_escalar.jsp','_parent');");
-                out.print("</script>");
-                }
-            }
-            if(request.getParameter("n") != null){
-                try{
-                n = Double.parseDouble(request.getParameter("n"));
-                }catch(Exception e){
-                erro = 1;
-                out.print("<script language='JavaScript'>");
-                out.print(" alert('Caracteres proibidos detectados!');");
-                out.print(" window.open('altera_escalar.jsp','_parent');");
-                out.print("</script>");
-                }
-            }
+    protected void processRequest(HttpServletRequest request,
+				  HttpServletResponse response)
+	    throws ServletException, IOException {
+	response.setContentType("text/html;charset=UTF-8");
+	PrintWriter out = response.getWriter();
+	HttpSession session = request.getSession();
+	try {
+	    /* TODO output your page here. You may use following sample code. */
+	    out.println("<html>");
+	    out.println("<head>");
+	    out.println("<title>Servlet EscalarMatriz</title>");
+	    out.println("</head>");
+	    out.println("<body>");
+	    int i, j, dima = 0, dimb = 0, erro = 0;
+	    double n = 0;
+	    if (request.getParameter("dima") != null) {
+		try {
+		    dima = Integer.parseInt(request.getParameter("dima"));
+		} catch (Exception e) {
+		    erro = 1;
+		    out.print("<script language='JavaScript'>");
+		    out.print(" alert('Caracteres proibidos detectados!');");
+		    out.print(" window.open('altera_escalar.jsp','_parent');");
+		    out.print("</script>");
+		}
+	    }
+	    if (request.getParameter("dimb") != null) {
+		try {
+		    dimb = Integer.parseInt(request.getParameter("dimb"));
+		} catch (Exception e) {
+		    erro = 1;
+		    out.print("<script language='JavaScript'>");
+		    out.print(" alert('Caracteres proibidos detectados!');");
+		    out.print(" window.open('altera_escalar.jsp','_parent');");
+		    out.print("</script>");
+		}
+	    }
+	    if (request.getParameter("n") != null) {
+		try {
+		    n = Double.parseDouble(request.getParameter("n"));
+		} catch (Exception e) {
+		    erro = 1;
+		    out.print("<script language='JavaScript'>");
+		    out.print(" alert('Caracteres proibidos detectados!');");
+		    out.print(" window.open('altera_escalar.jsp','_parent');");
+		    out.print("</script>");
+		}
+	    }
 
+	    double a[][] = new double[dima][dimb];
+	    double resultado[][];
 
-            double a[][] = new double[dima][dimb];
-            double resultado[][];
-            
-            for(i=0;i<dima;i++){
-                for(j=0;j<dimb;j++){
-            if(request.getParameter("a"+i+j) != null&& request.getParameter("a"+i+j) != ""){
-                 try{
-                a[i][j] = Double.parseDouble(request.getParameter("a"+i+j));
-                }catch(Exception e){
-                erro = 1;
-                out.print("<script language='JavaScript'>");
-                out.print(" alert('Caracteres proibidos detectados!');");
-                out.print(" window.open('altera_escalar.jsp','_parent');");
-                out.print("</script>");
-                }
-                }else
-            { a[i][j] = 0;}
-            }
-            }
-            session.setAttribute("dados_escalar_a", a);
-            session.setAttribute("dados_escalar_dima", dima);
-            session.setAttribute("dados_escalar_dimb", dimb);
-            session.setAttribute("dados_escalar_n", n);
-             if(erro == 0){
-            Escalar e = new Escalar(a, n, dima, dimb);
-            e.calcular();
-            resultado = e.getResultado();
-            session.setAttribute("resultado_escalar", resultado);
-            session.setAttribute("resultado_escalar_dima", dima);
-            session.setAttribute("resultado_escalar_dimb", dimb);
-            try {
-                e.setUsuario((Usuario)session.getAttribute("user"));
-                Usuario uP = e.getUsuario();
-                if(uP.temPermissao("/Facilita/listar_calculo.jsp","/Facilita",uP)){
-                CalculoDAO cDB = new CalculoDAO();
-                cDB.conectar();
-                if(request.getParameter("id")!=null){
-                e.setId(Integer.parseInt(request.getParameter("id")));
-                cDB.alterar(e);
-                }else{
-                cDB.inserir(e);
-                }
-                cDB.desconectar();    
-                }
-                } catch (Exception x) {
-            }
-            out.print("<script language='JavaScript'>");
-            out.print(" window.open('resultado_escalar.jsp','_parent');");
-            out.print("</script>");
-            }
-            out.println("</body>");
-            out.println("</html>");
-        } finally {            
-            out.close();
-        }
+	    for (i = 0; i < dima; i++) {
+		for (j = 0; j < dimb; j++) {
+		    if (request.getParameter("a" + i + j) != null
+			    && request.getParameter("a" + i + j) != "") {
+			try {
+			    a[i][j] = Double.parseDouble(request
+				    .getParameter("a" + i + j));
+			} catch (Exception e) {
+			    erro = 1;
+			    out.print("<script language='JavaScript'>");
+			    out.print(" alert('Caracteres proibidos detectados!');");
+			    out.print(" window.open('altera_escalar.jsp','_parent');");
+			    out.print("</script>");
+			}
+		    } else {
+			a[i][j] = 0;
+		    }
+		}
+	    }
+	    session.setAttribute("dados_escalar_a", a);
+	    session.setAttribute("dados_escalar_dima", dima);
+	    session.setAttribute("dados_escalar_dimb", dimb);
+	    session.setAttribute("dados_escalar_n", n);
+	    if (erro == 0) {
+		Escalar e = new Escalar(a, n, dima, dimb);
+		e.calcular();
+		resultado = e.getResultado();
+		session.setAttribute("resultado_escalar", resultado);
+		session.setAttribute("resultado_escalar_dima", dima);
+		session.setAttribute("resultado_escalar_dimb", dimb);
+		try {
+		    e.setUsuario((Usuario) session.getAttribute("user"));
+		    Usuario uP = e.getUsuario();
+		    if (uP.temPermissao("/Facilita/listar_calculo.jsp",
+			    "/Facilita", uP)) {
+			CalculoDAO cDB = new CalculoDAO();
+			cDB.conectar();
+			if (request.getParameter("id") != null) {
+			    e.setId(Integer.parseInt(request.getParameter("id")));
+			    cDB.alterar(e);
+			} else {
+			    cDB.inserir(e);
+			}
+			cDB.desconectar();
+		    }
+		} catch (Exception x) {
+		}
+		out.print("<script language='JavaScript'>");
+		out.print(" window.open('resultado_escalar.jsp','_parent');");
+		out.print("</script>");
+	    }
+	    out.println("</body>");
+	    out.println("</html>");
+	} finally {
+	    out.close();
+	}
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // <editor-fold defaultstate="collapsed"
+// desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
+     * Handles the HTTP <code>GET</code> method.
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    protected void doGet(HttpServletRequest request,
+			 HttpServletResponse response) throws ServletException,
+	    IOException {
+	processRequest(request, response);
     }
 
     /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
+     * Handles the HTTP <code>POST</code> method.
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    protected void doPost(HttpServletRequest request,
+			  HttpServletResponse response)
+	    throws ServletException, IOException {
+	processRequest(request, response);
     }
 
     /**
      * Returns a short description of the servlet.
-     *
+     * 
      * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
-        return "Short description";
+	return "Short description";
     }// </editor-fold>
 }
