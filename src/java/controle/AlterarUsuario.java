b39ec8eb2250159a29581de84ed93b88ea2c5f2b
Gabriel Araújo <gabiru1296@gmail.com>
2014-03-28 21:59:50 -0300

    Added headers to control classes.

--- src/java/controle/AlterarUsuario.java
+++ src/java/controle/AlterarUsuario.java
@@ -1,6 +1,8 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *Licensed under ..., see LICENSE.md
+ *Authors: André Bernardes.
+ *Created on: 28/03/2014, 11:23:34
+ *Description: Class that change the user.
  */
 
 package controle;
@@ -17,10 +19,7 @@ import modelo.PerfilDAO;
 import modelo.Usuario;
 import modelo.UsuarioDAO;
 
-/**
- *
- * @author André
- */
+
 public class AlterarUsuario extends HttpServlet {
    
     /** 
