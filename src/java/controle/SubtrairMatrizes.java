12921f7491210eb3a3794696d515d011bb764cb8
Gabriel Araújo <gabiru1296@gmail.com>
2014-03-28 22:26:09 -0300

    Added headers to control classes.

--- src/java/controle/SubtrairMatrizes.java
+++ src/java/controle/SubtrairMatrizes.java
@@ -1,6 +1,8 @@
 /*
- * To change this template, choose Tools | Templates and open the template in
- * the editor.
+ *Licensed under ..., see LICENSE.md
+ *Authors: André Bernardes.
+ *Created on: 28/03/2014, 11:23:34
+ *Description: Class to insert data to subtract matrices. 
  */
 package controle;
 
@@ -15,10 +17,7 @@ import modelo.CalculoDAO;
 import modelo.Subtrair;
 import modelo.Usuario;
 
-/**
- * 
- * @author Andre
- */
+
 public class SubtrairMatrizes extends HttpServlet {
 
     /**
