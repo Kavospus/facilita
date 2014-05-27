5a2ed7b9455ce2ef424016dff4f78b51933145e8
Guilherme da Luz <guilherme.daluz@outlook.com>
2014-05-26 22:41:29 -0300

    Decomposição em funç atômicas Classe LeastSquares

--- src/java/modelo/LeastSquares.java
+++ src/java/modelo/LeastSquares.java
@@ -56,42 +56,14 @@ public class LeastSquares {
 
 	    case 1:
 		// Linear
-		for (i = 0; i < qnt; i++) {
-		    A.set(0, 0, A.get(0, 0) + Math.pow(vx[i], 2));
-		    A.set(0, 1, A.get(0, 1) + vx[i]);
-		    A.set(1, 0, A.get(1, 0) + vx[i]);
-		    A.set(1, 1, A.get(1, 1) + 1);
-		}
-		for (i = 0; i < qnt; i++) {
-		    b.set(0, 0, b.get(0, 0) + vx[i] * vy[i]);
-		    b.set(1, 0, b.get(1, 0) + vy[i]);
-		}
-		x = solveMatrix(A, b, dimens);
-
+                x = linearAdjust(A, b, dimens, qnt, vx, vy);
 		for (i = 0; i < dimens; i++) {
 		    resultLeastSquares[i] = x.get(i, 0);
 		}
 		break;
 	    case 2:
 		// Gauss
-		for (i = 0; i < qnt; i++) {
-		    A.set(0, 0, A.get(0, 0) + 1);
-		    A.set(0, 1, A.get(0, 1) + vx[i]);
-		    A.set(0, 2, A.get(0, 2) + Math.pow(vx[i], 2));
-		    A.set(1, 0, A.get(1, 0) + vx[i]);
-		    A.set(1, 1, A.get(1, 1) + Math.pow(vx[i], 2));
-		    A.set(1, 2, A.get(1, 2) + Math.pow(vx[i], 3));
-		    A.set(2, 0, A.get(2, 0) + Math.pow(vx[i], 2));
-		    A.set(2, 1, A.get(2, 1) + Math.pow(vx[i], 3));
-		    A.set(2, 2, A.get(2, 2) + Math.pow(vx[i], 4));
-		}
-		for (i = 0; i < qnt; i++) {
-		    b.set(0, 0, b.get(0, 0) + Math.log(vy[i]));
-		    b.set(1, 0, b.get(1, 0) + vx[i] * Math.log(vy[i]));
-		    b.set(2, 0,
-			    b.get(2, 0) + Math.pow(vx[i], 2) * Math.log(vy[i]));
-		}
-		x = solveMatrix(A, b, dimens);
+                x = gaussianAdjust(A, b, dimens, qnt, vx, vy);
 		reverteLn[1] = x.get(2, 0);
 		reverteLn[2] = x.get(1, 0) / (2 * reverteLn[1]);
 		reverteLn[0] = Math.exp(x.get(0, 0) - reverteLn[1]
@@ -103,7 +75,43 @@ public class LeastSquares {
 		break;
 	    case 3:
 		// Parabolico
-		for (i = 0; i < qnt; i++) {
+                x = parabolicAdjust(A, b, dimens, qnt, vx, vy);
+		for (i = 0; i < dimens; i++) {
+		    resultLeastSquares[i] = x.get(i, 0);
+		}
+		break;
+	    case 4:
+		// Exponencial                
+                x = expAdjust(A, b, dimens, qnt, vx, vy);
+		reverteLn[0] = Math.exp(x.get(0, 0));
+		reverteLn[1] = x.get(1, 0);
+		for (i = 0; i < dimens; i++) {
+		    resultLeastSquares[i] = reverteLn[i];
+		}
+		break;
+	}
+
+	return resultLeastSquares;
+    }
+    
+    SimpleMatrix linearAdjust(SimpleMatrix A, SimpleMatrix b, int dimens, 
+            int qnt, double vx[], double vy[]) {
+        for (int i = 0; i < qnt; i++) {
+		    A.set(0, 0, A.get(0, 0) + Math.pow(vx[i], 2));
+		    A.set(0, 1, A.get(0, 1) + vx[i]);
+		    A.set(1, 0, A.get(1, 0) + vx[i]);
+		    A.set(1, 1, A.get(1, 1) + 1);
+		}
+		for (int i = 0; i < qnt; i++) {
+		    b.set(0, 0, b.get(0, 0) + vx[i] * vy[i]);
+		    b.set(1, 0, b.get(1, 0) + vy[i]);
+		}
+		return solveMatrix(A, b, dimens);
+    }
+    
+    SimpleMatrix gaussianAdjust(SimpleMatrix A, SimpleMatrix b, int dimens,
+            int qnt, double vx[], double vy[]) {
+        for (int i = 0; i < qnt; i++) {
 		    A.set(0, 0, A.get(0, 0) + 1);
 		    A.set(0, 1, A.get(0, 1) + vx[i]);
 		    A.set(0, 2, A.get(0, 2) + Math.pow(vx[i], 2));
@@ -114,39 +122,52 @@ public class LeastSquares {
 		    A.set(2, 1, A.get(2, 1) + Math.pow(vx[i], 3));
 		    A.set(2, 2, A.get(2, 2) + Math.pow(vx[i], 4));
 		}
-		for (i = 0; i < qnt; i++) {
+		for (int i = 0; i < qnt; i++) {
+		    b.set(0, 0, b.get(0, 0) + Math.log(vy[i]));
+		    b.set(1, 0, b.get(1, 0) + vx[i] * Math.log(vy[i]));
+		    b.set(2, 0,
+			    b.get(2, 0) + Math.pow(vx[i], 2) * Math.log(vy[i]));
+		}
+                
+                return solveMatrix(A, b, dimens);
+    }
+    
+    SimpleMatrix parabolicAdjust(SimpleMatrix A, SimpleMatrix b, int dimens,
+            int qnt, double vx[], double vy[]){
+        for (int i = 0; i < qnt; i++) {
+		    A.set(0, 0, A.get(0, 0) + 1);
+		    A.set(0, 1, A.get(0, 1) + vx[i]);
+		    A.set(0, 2, A.get(0, 2) + Math.pow(vx[i], 2));
+		    A.set(1, 0, A.get(1, 0) + vx[i]);
+		    A.set(1, 1, A.get(1, 1) + Math.pow(vx[i], 2));
+		    A.set(1, 2, A.get(1, 2) + Math.pow(vx[i], 3));
+		    A.set(2, 0, A.get(2, 0) + Math.pow(vx[i], 2));
+		    A.set(2, 1, A.get(2, 1) + Math.pow(vx[i], 3));
+		    A.set(2, 2, A.get(2, 2) + Math.pow(vx[i], 4));
+		}
+		for (int i = 0; i < qnt; i++) {
 		    b.set(0, 0, b.get(0, 0) + vy[i]);
 		    b.set(1, 0, b.get(1, 0) + vx[i] * vy[i]);
 		    b.set(2, 0, b.get(2, 0) + Math.pow(vx[i], 2) * vy[i]);
 		}
-		x = solveMatrix(A, b, dimens);
-
-		for (i = 0; i < dimens; i++) {
-		    resultLeastSquares[i] = x.get(i, 0);
-		}
-		break;
-	    case 4:
-		// Exponencial
-		for (i = 0; i < qnt; i++) {
+                
+		return solveMatrix(A, b, dimens);
+    }
+    
+    SimpleMatrix expAdjust(SimpleMatrix A, SimpleMatrix b, int dimens,
+            int qnt, double vx[], double vy[]) {
+        for (int i = 0; i < qnt; i++) {
 		    A.set(0, 0, A.get(0, 0) + 1);
 		    A.set(0, 1, A.get(0, 1) + vx[i]);
 		    A.set(1, 0, A.get(1, 0) + vx[i]);
 		    A.set(1, 1, A.get(1, 1) + Math.pow(vx[i], 2));
 		}
-		for (i = 0; i < qnt; i++) {
+		for (int i = 0; i < qnt; i++) {
 		    b.set(0, 0, b.get(0, 0) + Math.log(vy[i]));
 		    b.set(1, 0, b.get(1, 0) + vx[i] * Math.log(vy[i]));
 		}
-		x = solveMatrix(A, b, dimens);
-		reverteLn[0] = Math.exp(x.get(0, 0));
-		reverteLn[1] = x.get(1, 0);
-		for (i = 0; i < dimens; i++) {
-		    resultLeastSquares[i] = reverteLn[i];
-		}
-		break;
-	}
-
-	return resultLeastSquares;
+		
+                return solveMatrix(A, b, dimens);
     }
     /**
      *
