6d82a13c753ed5363e003052559a50ed4ba8188b
Luís Filipe Resende Vilela <luisfrvilela@gmail.com>
2014-06-04 05:59:01 -0300

    Adding more tests to ScaleTest

--- test/tests/ScaleTest.java
+++ test/tests/ScaleTest.java
@@ -18,7 +18,7 @@ public class ScaleTest {
     
     public ScaleTest() {
     }
-    Scale escalar;
+    Scale scale;
     @Before
     public void setUp() {
     }
@@ -28,12 +28,28 @@ public class ScaleTest {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
+    
     @Test
-    public void testEscalar() {
-        escalar = new Scale(new double[][]{{1.0,2.0},{3.0,2.0}},-0.25, 2, 2);
-        escalar.calculate();
-        double[][] saida = new double[][]{{-0.25,-0.5},{-0.75,-0.5}};
-        assertArrayEquals(saida[0],escalar.getResult()[0], 0.000001);
-        assertArrayEquals(saida[1],escalar.getResult()[1], 0.000001);
+    public void testInvertFirstValue(){
+        testScale(new double[][]{{1.0,2.0},{3.0,2.0}}, -0.25, 2,2, new double[][]{{-0.25,-0.5},{-0.75,-0.5}});
+    }
+    
+    @Test
+    public void testInvertSecondValue(){
+        testScale(new double[][]{{1.0,2.0},{3.0,4.0}}, 1000.0, 2,2, new double[][]{{1000,2000},{3000,4000}});
+    }
+    
+    @Test
+    public void testInvertThirdValue(){
+        testScale(new double[][]{{-1.0,2.0},{3.0,-4.0}}, -1.0, 2,2, new double[][]{{1.0,-2.0},{-3.0,4.0}});
+    }
+    
+    private void testScale(double[][] inputA, double inputB, int linesA,
+	    int columnsA, double [][] valueExpected) {
+        scale = new Scale(inputA, inputB, linesA, columnsA);
+        scale.calculate();
+        double[][] saida = valueExpected;
+        assertArrayEquals(saida[0],scale.getResult()[0], 0.000001);
+        assertArrayEquals(saida[1],scale.getResult()[1], 0.000001);
     }
 }
