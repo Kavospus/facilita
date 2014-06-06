569df6e781126a7b7df7b87d1f210e60d4cb0104
Luís Filipe Resende Vilela <luisfrvilela@gmail.com>
2014-06-06 07:57:30 -0300

    Create class LeastSquaresTest

---
+++ test/tests/LeastSquaresTest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under ..., see LICENSE.md
+ * Authors:
+ * Created on: 06/06/2014, 07:56:41
+ * Description:
+ */
+
+package tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author luis
+ */
+public class LeastSquaresTest {
+    
+    public LeastSquaresTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
